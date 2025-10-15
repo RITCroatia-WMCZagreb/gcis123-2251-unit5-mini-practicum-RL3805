59c1cc17d4fe4d9e592c4e04facd3206dbd1714a
Roko Lovrecek (RIT Student) <rl3805@rit.edu>
2025-10-15 12:48:13 +0200

    GIT

--- pi_tester.py
+++ pi_tester.py
@@ -1,21 +1,32 @@
 '''
-@ASSESSME.USERID: userID
-@ASSESSME.AUTHOR: author, list of authors
+@ASSESSME.USERID: rl3805
+@ASSESSME.AUTHOR: Roko Lovrecek
 @ASSESSME.DESCRIPTION: MiniPractical
 @ASSESSME.ANALYZE: YES
 '''
 
 # The  thecima values of PI
-PI_VALUE = ""
+PI_VALUE = "1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679"
 
 def pi_tester():
-    pass
+    digit = input("Enter the digit:")
+    index = PI_VALUE.split
+    print (index)
+    
+    
+    
+
+
+
+
+
+
 
 def display_score(score):
     pass
 
 def main():
-    pass
+    pi_tester()
 
 if __name__ == "__main__":
     main()
